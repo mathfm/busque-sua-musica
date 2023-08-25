96b4d35c6b090f45ca0d919f1a5ec1da8c57beee
mathfm <fernandomath.mds@gmail.com>
2023-08-25 06:35:36 -0300

    logica de busca finalizada

--- script.js
+++ script.js
@@ -1,28 +1,52 @@
+const forms = document.getElementById("buscar-msc");
+const listaDeMusicas = document.querySelector(".conteudo-tabela");
 
+forms.addEventListener("submit", (form) => {
+    form.preventDefault();
+    buscarResultado();
+})
+function buscarResultado() {
+    const cantor = document.querySelector(".buscar");
+    const url = `https://api.lyrics.ovh/suggest/${cantor.value}`;
+    listaDeMusicas.innerHTML = "";
+    getMusica(url);
+}
+async function getMusica(url) {
+    const response = await fetch(url).then(resposta => resposta.json());
+    const data = response.data;
+    
+    for (const { title, album: { cover_medium, title: tituloDoAlbum }, artist: { name } } of data) {
+    
+    const conteudoTabela = document.createElement("tr");
+    const infoArtista = document.createElement("td");
+    const infoMusica = document.createElement("td");
+    const imgDoAlbum = document.createElement("img");
+    const tabelaCapa = document.createElement("td");
+    const tabelaAlbum = document.createElement("td");
+    
+    tabelaAlbum.innerText = tituloDoAlbum;
+    infoMusica.innerText = title;
+    infoArtista.innerText = name;
+    imgDoAlbum.src = cover_medium;
+    imgDoAlbum.width = 100;
+    imgDoAlbum.alt = "Imagem da capa do album";
 
-/*
-const url = "https://jsonplaceholder.typicode.com/posts"
-async function getComentarios() {
-    const data = await fetch(url).then(resposta => resposta.json());
-    const divComentarios = document.querySelector("#idcomentarios");
+    tabelaCapa.appendChild(imgDoAlbum);
+    conteudoTabela.appendChild(tabelaCapa);
+    conteudoTabela.appendChild(infoMusica);
+    conteudoTabela.appendChild(infoArtista);
+    conteudoTabela.appendChild(tabelaAlbum);
+    listaDeMusicas.appendChild(conteudoTabela);
 
-    data.map((coments => {
-        const paragrafo = document.createElement("p");
-        paragrafo.classList.add("comentarios");
-        paragrafo.textContent = coments.title;
-        divComentarios.appendChild(paragrafo);
-    }))
+    } 
+    
 }
 
-*/
-const cantor = "Um Barril de Rap";
-const url = `https://api.lyrics.ovh/suggest/${cantor}`;
 
-async function getMusica() {
-    const dats = await fetch(url).then(resposta => resposta.json());
 
-    console.log(dats);
 
-}
 
-//getMusica();
+
+
+
+
